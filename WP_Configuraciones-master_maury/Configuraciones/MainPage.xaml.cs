e72f9ddfce6afc4899bf396f4ab039ea118d257f
jdmaury <jdmaury@hotmail.com>
2015-03-09 12:19:56 -0500

    ConfiguracionWP
    Guarda tus datos

---
+++ WP_Configuraciones-master_maury/Configuraciones/MainPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using System.IO.IsolatedStorage;
+using Microsoft.Phone.Shell;
+
+namespace Configuraciones
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public IsolatedStorageSettings appSettings;
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            
+            appSettings = IsolatedStorageSettings.ApplicationSettings;
+
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            try
+            {
+                user_test.Text = appSettings["user"].ToString();
+                user_test1.Text = appSettings["user1"].ToString();
+                
+                radioButton1.IsChecked = (bool)appSettings["user2"];
+                radioButton2.IsChecked = (bool)appSettings["user3"];
+                //user_test2.Text = appSettings["user2"].ToString();
+                
+            }
+            catch (Exception)
+            {
+                appSettings.Add("user", "User");
+                appSettings.Add("user2", false);
+                appSettings.Add("user3", false);
+                appSettings.Add("user1", "Edad");                
+                
+                radioButton1.IsChecked = false;
+                radioButton2.IsChecked = false;
+                //user_test2.Text = "Masculino";
+                
+                appSettings.Save();
+            }
+        }
+
+        protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            appSettings["user"] = user_test.Text;
+            appSettings["user1"] = user_test1.Text;
+            //appSettings["user2"] = user_test2.Text;
+            appSettings["user2"] = radioButton1.IsChecked;
+            appSettings["user3"] = radioButton2.IsChecked;
+            appSettings.Save();
+            PhoneApplicationService.Current.State["Text"] = user_test.Text;
+            PhoneApplicationService.Current.State["Text1"] = user_test1.Text;
+
+            if (radioButton1.IsChecked == true)
+            {
+                PhoneApplicationService.Current.State["Text2"] = radioButton1.Content;
+            }
+            else
+            {
+                if (radioButton2.IsChecked == true)
+                {
+                    PhoneApplicationService.Current.State["Text2"] = radioButton2.Content;
+                }
+            }
+            
+            
+            //PhoneApplicationService.Current.State["Text2"] = user_test2.Text;
+        }
+
+    }
+}
---
+++ WP_Configuraciones-master_maury/Configuraciones/paginaprincipal.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.IO.IsolatedStorage;
+namespace Configuraciones
+{
+    public partial class paginaprincipal : PhoneApplicationPage
+    {
+        public IsolatedStorageSettings appSettings;
+
+        public paginaprincipal()
+        {
+            InitializeComponent();
+            appSettings = IsolatedStorageSettings.ApplicationSettings;
+        }
+    
+                   protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+                   {
+                       try
+                            {
+                                   textBox1.Text = appSettings["user"].ToString();
+                                   textBox2.Text = appSettings["user1"].ToString();
+                                   if(appSettings["user2"].ToString().Equals("True"))
+                                   textBox3.Text = "Masculino";
+                                    if(appSettings["user2"].ToString().Equals("False"))
+                                   textBox3.Text = "Femenino";
+                                
+                            }
+                       catch(Exception){
+                                               textBox1.Text = "default_value";
+                                               textBox2.Text = "default_value";
+                                               textBox3.Text = "default_value";
+                                               appSettings.Add("user", "Usuario");
+                                               appSettings.Add("user2", false);
+                                               appSettings.Add("user3", false);
+                                               appSettings.Add("user1", "Edad");                
+                                               appSettings.Save();
+                
+                       }
+                   
+        }
+    
+        private void navegar(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
+
+        }
+
+        /*protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            if (PhoneApplicationService.Current.State.ContainsKey("Text"))
+                textBox1.Text = (string)PhoneApplicationService.Current.State["Text"];
+            if (PhoneApplicationService.Current.State.ContainsKey("Text1"))
+                textBox2.Text = (string)PhoneApplicationService.Current.State["Text1"];
+            if (PhoneApplicationService.Current.State.ContainsKey("Text2") )
+                textBox3.Text = (string)PhoneApplicationService.Current.State["Text2"];
+            
+        }*/
+
+        protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            appSettings["user"] = textBox1.Text;
+            appSettings["user1"] = textBox2.Text;
+            //appSettings["user2"] = user_test2.Text;
+            
+            appSettings.Save();
+            PhoneApplicationService.Current.State["Text"] = textBox1.Text;
+            PhoneApplicationService.Current.State["Text1"] =textBox2.Text;
+
+            
+            
+            //PhoneApplicationService.Current.State["Text2"] = user_test2.Text;
+        }
+        
+    }
+}
